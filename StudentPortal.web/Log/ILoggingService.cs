9e435a35e91a6f51f7f258daae315b4b5141f2d7
Ameenul Firdous <127436359+d3ku010@users.noreply.github.com>
2025-01-31 14:35:31 +0530

    1.2

---
+++ StudentPortal.web/Log/ILoggingService.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentPortal.web.Log
+{
+    public interface ILoggingService
+    {
+        void LogInfo(string message);
+        void LogError(string message, Exception? ex=null);
+        void LogWarn(string message);
+        void LogDebug(string message);
+    }
+}
---
+++ StudentPortal.web/Log/LoggingService.cs
@@ -0,0 +1,38 @@
+﻿using NLog;
+namespace StudentPortal.web.Log
+{
+    public class LoggingService : ILoggingService
+    {
+        private static readonly NLog.ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public void LogInfo(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void LogWarn(string message)
+        {
+            logger.Warn(message);
+        }
+
+        public void LogError(string message, Exception? ex = null)
+        {
+            if (ex != null)
+            {
+                logger.Error(ex, message);
+            }
+            else
+            {
+                logger.Error(message);
+            }
+
+        }
+
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+
+
+    }
+}
